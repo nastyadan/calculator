659cb032422fe2b31a09f4cf99aeeafbe05cf4e6
Anastasia Danilova <anastasia.proekt@gmail.com>
2022-09-03 02:55:54 +0300

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(){
+let a = Number(prompt('введите число'));
+let b = Number(prompt('введите число'));
+
+alert(a + b);
+}
+
+function subtract(){
+    let a = Number(prompt('введите число'));
+    let b = Number(prompt('введите число'));
+    
+    alert(a - b);
+    }
+
+function multiply(){
+    let a = Number(prompt('введите число'));
+    let b = Number(prompt('введите число'));
+        
+    alert(a * b);
+}
+
+function divide(){
+    let a = Number(prompt('введите число'));
+    let b = Number(prompt('введите число'));
+            
+    alert(a / b);
+}
